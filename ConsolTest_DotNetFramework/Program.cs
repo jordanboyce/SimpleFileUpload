66e067d10c5b409025d87ce3da119e8caeb2b961
Jordan Boyce <Jordan.Boyce@inl.gov>
2022-09-12 23:42:02 -0600

    Was able to upload file to endpoint using byte array and form

--- ConsolTest_DotNetFramework/Program.cs
+++ ConsolTest_DotNetFramework/Program.cs
@@ -5,57 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ConsolTest_DotNetFramework
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    internal class Program
     {
-      string res = SaveTest(@"c:\temp\NewSimResults.txt");
-      Console.WriteLine(res);
-    }
-
-    public static string SaveTest(string filePath)
-    {
-      try
-      {
-        using (FileStream solveStream = File.Open(filePath, FileMode.Open))
+        static void Main(string[] args)
         {
-          using (var httpClient = new HttpClient())
-          {
-            httpClient.Timeout = TimeSpan.FromHours(6);
-            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://localhost:7198/api/uploadfile")) 
-            {
-              var multipartContent = new MultipartFormDataContent();
-              multipartContent.Add(new StreamContent(solveStream), "file");
-              request.Content = multipartContent;
-
-              var task = Task.Run(() => httpClient.SendAsync(request));
-              var response = task.Result;
-
-              if (response.IsSuccessStatusCode)
-              {
-                var responseContent = response.Content;
-
-                // by calling .Result you are synchronously reading the result
-                //Testing
-                string responseString = responseContent.ReadAsStringAsync().Result;
-                return responseString;
-              }
-              else
-              {
-                return "Error" + response.StatusCode.ToString();
-              }
-            }
-          }
+            string res = SaveTest(@"C:\Users\BOYCJT\Pictures\testFile2.docx");
+            Console.WriteLine(res);
         }
-        return "";
-      }
-      catch (Exception e)
-      {
-        return "Exception - " + e.Message;
-      }
+
+        public static string SaveTest(string filePath)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var form = new MultipartFormDataContent();
+
+                    byte[] fileData = File.ReadAllBytes(filePath);
+
+                    ByteArrayContent byteContent = new ByteArrayContent(fileData);
+
+                    byteContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+                    form.Add(byteContent, "file", Path.GetFileName(filePath));
+
+                    var result = httpClient.PostAsync("https://localhost:7198/api/uploadfile", form).ConfigureAwait(false).GetAwaiter().GetResult().Content.ReadAsStringAsync().Result;
+
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        return "Result is null";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return "Exception - " + e.Message;
+            }
+        }
     }
-  }
 }
